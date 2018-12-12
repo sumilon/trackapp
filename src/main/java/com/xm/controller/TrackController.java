a1ca3004b9e3538556ee46706c13e35f68a20547
Sumilon <sumilon@Sumilons-MacBook-Air.local>
2018-12-12 22:55:21 +0530

    first commit

--- src/main/java/com/xm/controller/TrackController.java
+++ src/main/java/com/xm/controller/TrackController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xm.model.Track;
 import com.xm.repository.TrackRepository;
 
 @RestController
+@RequestMapping("/track")
 public class TrackController {
 
 	@Autowired
